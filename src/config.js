5df787e4ba1e81e044ef291a38bda30f7a1d7cc9
WHATCOST99 <131562521+OladinsNft@users.noreply.github.com>
2023-08-12 17:20:12 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // # Chain config
 export const NEXT_PUBLIC_COINFLIP_CONTRACT=""
-export const NEXT_PUBLIC_CHAIN_ID="Injective-888"
-export const NEXT_PUBLIC_CHAIN_NAME="Injective Mainnet"
+export const NEXT_PUBLIC_CHAIN_ID="injective-888"
+export const NEXT_PUBLIC_CHAIN_NAME="Injective"
 export const NEXT_PUBLIC_CHAIN_BECH32_PREFIX="inj"
 export const NEXT_PUBLIC_CHAIN_RPC_ENDPOINT="https://tm.injective.network/"
 export const NEXT_PUBLIC_CHAIN_REST_ENDPOINT="https://tm.injective.network/"
